24c82cba495dee39a0acdedb983b2091fcd47743
oneir0313 <oneir0313@gmail.com>
2021-10-05 11:14:05 +0800

    add unique path II other solution

--- problems/UniquePaths.go
+++ problems/UniquePaths.go
@@ -2,35 +2,35 @@ package problems
 
 func (p Problem) UniquePaths(m int, n int) int {
 	left := 1
-    up := make([]int, m)
-    
-    for i := range up {
-        up[i] = 1
-    }
-    
-    for i := 1; i < n; i++ {
-        for j := 1; j < m; j++ {
-            up[j] = up[j] + left
-            left = up[j]
-        }
-        left = 1
-    }
-    
-    return up[m - 1]
+	up := make([]int, m)
+
+	for i := range up {
+		up[i] = 1
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < m; j++ {
+			up[j] = up[j] + left
+			left = up[j]
+		}
+		left = 1
+	}
+
+	return up[m-1]
 }
 
-func UniquePathsSoutionII(m int, n int) uint64 {
+func UniquePathsSolutionII(m int, n int) uint64 {
 	if m >= n {
-		return factorialTo(uint64(m + n -2), uint64(m-1)) / factorial(uint64(n-1))
+		return factorialTo(uint64(m+n-2), uint64(m-1)) / factorial(uint64(n-1))
 	} else {
-		return factorialTo(uint64(m + n -2), uint64(n-1)) / factorial(uint64(m-1))
+		return factorialTo(uint64(m+n-2), uint64(n-1)) / factorial(uint64(m-1))
 	}
 }
 
 func factorial(n uint64) uint64 {
 	var res uint64
-	if (n > 0) {
-		res =  uint64(n * factorial(n-1))
+	if n > 0 {
+		res = uint64(n * factorial(n-1))
 		return res
 	}
 	return 1
@@ -41,8 +41,8 @@ func factorialTo(n uint64, m uint64) uint64 {
 		return 1
 	}
 	var res uint64
-	if (n > 0) {
-		res =  uint64(n * factorialTo(n-1, m))
+	if n > 0 {
+		res = uint64(n * factorialTo(n-1, m))
 		return res
 	}
 	return 1
--- problems/UniquePathsWithObstacles.go
+++ problems/UniquePathsWithObstacles.go
@@ -41,3 +41,42 @@ func (p Problem) UniquePathsWithObstacles(obstacleGrid [][]int) int {
     }
     return result[m-1][n-1]
 }
+
+
+// refer to official solution
+func(p Problem) UniquePathsWithObstaclesSolutionII(obstacleGrid [][]int) int {
+    m := len(obstacleGrid)
+    n := len(obstacleGrid[0])
+    if obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1{
+        return 0
+    }
+    
+    obstacleGrid[0][0] = 1
+
+    for i:=1; i<m; i++{
+        obstacleGrid[i][0] = b2i(obstacleGrid[i][0] == 0 && obstacleGrid[i-1][0] == 1)
+    }
+    
+    for j:=1; j<n; j++{
+        obstacleGrid[0][j] = b2i(obstacleGrid[0][j] == 0 && obstacleGrid[0][j-1] == 1)
+    }
+    
+    
+    for i:=1;i<m; i++{
+        for j:=1; j<n; j++{
+            if obstacleGrid[i][j] == 0 {
+                obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+            } else {
+                obstacleGrid[i][j] = 0
+            }
+        }
+    }
+    return obstacleGrid[m-1][n-1]
+}
+
+func b2i(b bool) int {
+    if b {
+        return 1
+    }
+    return 0
+}
