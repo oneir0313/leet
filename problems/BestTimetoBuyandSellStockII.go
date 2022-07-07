0883025914b9aed4d817bf764e4a0a58010d2819
oneir0313 <oneir0313@gmail.com>
2022-07-07 13:47:13 +0800

    feat: add max profit follow up

--- problems/BestTimetoBuyandSellStockII.go
+++ problems/BestTimetoBuyandSellStockII.go
@@ -12,3 +12,14 @@ func (p Problem) MaxProfit(prices []int) int {
 	}
 	return profit
 }
+
+// 省去buy變數
+func (p Problem) MaxProfitFollowUp(prices []int) int {
+	profit := 0
+	for i := 0; i < len(prices)-1; i++ {
+		if prices[i+1] > prices[i] {
+			profit += prices[i+1] - prices[i]
+		}
+	}
+	return profit
+}
