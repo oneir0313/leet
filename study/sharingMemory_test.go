d3480a53fdc8879762dbbd5c0e24219d04683135
oneir0313 <oneir0313@gmail.com>
2022-11-17 20:39:20 +0800

    add study of sharing memory

---
+++ study/sharingMemory_test.go
@@ -0,0 +1,15 @@
+package study
+
+import "testing"
+
+func BenchmarkAddByShareMemory(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		addByShareMemory(100)
+	}
+}
+
+func BenchmarkAddByShareCommunicate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		addByShareCommunicate(100)
+	}
+}
